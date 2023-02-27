7e66810a1e70d008dd741117116eb4e6c7decbbb
ZiogasThlem <ziogas.thlem@gmail.com>
2023-02-27 15:45:52 +0200

    feat: added pages and services directories and their required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    PokemonCataloguePage,
+    TrainerPage
   ],
   imports: [
     BrowserModule,
---
+++ src/enviroments/enviroment.prod.ts
@@ -0,0 +1,6 @@
+export const enviroment = {
+    production: true,
+    trainerApiKey: "",
+    trainerApiUrl: "",
+    pokeApiUrl: ""
+}
